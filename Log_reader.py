f9dd91ee52521890fd6b58bb8c9519f68edbbbb3
shashankmarch27 <shashankmarch27@gmail.com>
2023-06-17 18:21:58 +0530

    added graph for throttle value

--- Log_reader.py
+++ Log_reader.py
@@ -3,11 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 flightLog = pd.read_csv('E:/log.csv')
-time = flightLog[['Time']]
+time = 0.000001 * flightLog[['Time']]
 altitude = flightLog[['Altitude']]
+throttle = flightLog[['Throttle']]
 plt.plot(time,altitude)
+plt.plot(time,throttle)
 plt.title("PID")
 plt.xlabel('time')
-plt.ylabel('altitude')
+plt.ylabel('altitude / throttle')
 plt.draw()
 plt.show()
