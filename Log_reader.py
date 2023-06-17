e9c669438b3906ef2ee9d1c40722fc92fe9b2318
rishi18722 <reeshavchowdhury18722@gmail.com>
2023-06-17 14:36:09 +0530

    PID_graph

--- Log_reader.py
+++ Log_reader.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-
-flightLog = pd.read_csv('E:\log.csv')
-
+flightLog = pd.read_csv('/Users/reeshavchowdhury/Downloads/log.csv')
+time = df[['Time']]
+altitude = df[['Altitude']]
+plt.plot(time,altitude)
+plt.title("PID")
+plt.xlabel('time')
+plt.ylabel('altitude')
+plt.draw()
+plt.show()
 print(flightLog)
